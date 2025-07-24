32551523348c16b0423e697dd57be4bdae0251da
shabaraan <amakm473@gmail.com>
2025-07-24 11:22:18 +0700

    berhasil fetch CPL berdasarkan ProdiId, KurikulumId dari API

--- src/utils/fetchCPL.js
+++ src/utils/fetchCPL.js
@@ -1,24 +1,6 @@
 // utils/fetchCPL.js
-export const fetchCPLData = async (prodiId, kurikulumId) => {
-    const response = await fetch('/api/proxy-cpl', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            ProdiId: 2,
-            KurikulumId: 2,
-        }),
-    });
 
-    if (!response.ok) {
-        throw new Error('Gagal mengambil data CPL');
-    }
-
-    return await response.json();
-};
-
-// Kode asli
+// KODE ASLI dengan Method POST dan request BODY
 // export const fetchCPLData = async (prodiId, kurikulumId) => {
 //     const response = await fetch('http://192.168.54.59:3001/api_obe/cpl/prodi/kurikulum', {
 //         method: 'POST',
@@ -37,3 +19,44 @@ export const fetchCPLData = async (prodiId, kurikulumId) => {
 
 //     return await response.json();
 // };
+
+// KODE DENGAN PROXY URL API untuk menghindari CORS
+// export const fetchCPLData = async (prodiId, kurikulumId) => {
+//     const response = await fetch('/api/proxy-cpl', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({
+//             ProdiId: 2,
+//             KurikulumId: 2,
+//         }),
+//     });
+
+//     if (!response.ok) {
+//         throw new Error('Gagal mengambil data CPL');
+//     }
+
+//     return await response.json();
+// };
+
+// KODE DENGAN URLSearchParams
+export const fetchCPLData = async (prodiId, kurikulumId) => {
+    const params = new URLSearchParams({
+        ProdiId: prodiId,
+        KurikulumId: kurikulumId,
+    });
+
+    const response = await fetch(`http://192.168.54.59:3001/api_obe/cpl/prodi/kurikulum?${params.toString()}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    if (!response.ok) {
+        throw new Error('Gagal mengambil data CPL');
+    }
+
+    return await response.json();
+};
